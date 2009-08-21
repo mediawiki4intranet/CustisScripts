8d6c23f0858dc65a54f0d89c1f7f7725ff7511ec
Vitaliy Filippov <vitalif@yourcmc.ru>
2009-08-21 17:47:16 +0000

    Bug 53254 List of pages in category (with closure)

--- CustisScripts.php
+++ CustisScripts.php
@@ -35,3 +35,56 @@ EOT;
 }
 
 $wgHooks['BeforePageDisplay'][] = 'wfAddCustisScriptsJS';
+$wgAjaxExportList[] = 'get_category_page_list';
+
+function efcustis_get_subcategories ($dbr, $dbkey)
+{
+    $cat = array();
+    $res = $dbr->select(
+        array('categorylinks', 'page'),
+        array('page_title'),
+        array(
+            'cl_from=page_id',
+            'cl_to' => $dbkey,
+            'page_namespace' => NS_CATEGORY
+        ),
+        __METHOD__
+    );
+    while ($row = $dbr->fetchRow($res))
+        $cat[] = $row[page_title];
+    $dbr->freeResult($res);
+    foreach ($cat as $c)
+        $cat = $cat + efcustis_get_subcategories ($dbr, $c);
+    return $cat;
+}
+
+function get_category_page_list ($categoryname)
+{
+    $cattitle = Title::newFromText($categoryname, NS_CATEGORY);
+    $dbr = wfGetDB(DB_SLAVE);
+    $cat = array($categoryname) + efcustis_get_subcategories($dbr, $cattitle->getDBkey());
+    $pages = array();
+    $res = $dbr->select(
+        array('categorylinks', 'page'),
+        array('page_title'),
+        array(
+            'cl_from=page_id',
+            'cl_to' => $cat,
+            'page_namespace' => NS_MAIN
+        ),
+        __METHOD__,
+        array('ORDER BY' => 'page_title')
+    );
+    while ($row = $dbr->fetchRow($res))
+        $pages[] = $row[page_title];
+    $dbr->freeResult($res);
+    if (count($pages))
+    {
+        foreach ($pages as $k => $p)
+            $pages[$k] = addcslashes($pages[$k], "'");
+        $pages = "['".join("','",$pages)."']";
+    }
+    else
+        $pages = "[]";
+    return $pages;
+}
